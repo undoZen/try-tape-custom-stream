15e9654c60263365707d2ee7174097d6841ea7dd
Anson Yu Wang <opensource@undozen.com>
2015-04-19 17:01:36 +0800

    use tape-bunyan-pub-stream

--- test.js
+++ test.js
@@ -1,29 +1,6 @@
 'use strict';
 var tape = require('tape');
-var PubStream;
-try {
-    PubStream = require('bunyan-pub-stream');
-} catch (e) {
-    try {
-        PubStream = require('bunyan-hub-logger/node_modules/bunyan-pub-stream');
-    } catch (e) {}
-} finally {
-    if (!PubStream) {
-        console.error('no bunyan-pub-stream module found');
-        process.exit(1);
-    }
-}
-var log = require('bunyan-hub-logger')({app: 'try-tape', name: 'test-result'});
-var ts = tape.createStream({objectMode: true});
-ts.on('data', function (row) {
-    log[row.type === 'assert' && !row.ok ? 'warn' : 'info']({tap: row});
-});
-ts.on('end', function () {
-    log.streams
-        .map(function (obj) { return obj.stream; })
-        .filter(function (stream) { return stream instanceof PubStream; })
-        .forEach(function (pubStream) { pubStream.end(); });
-});
+require('tape-bunyan-pub-stream')('try-tape-custom-stream');
 
 tape(function (test) {
     test.plan(2);
